02e0c716b9b7774a908b1e5a1bdf3561e4408a33
MuhammadSaadBadar <sp24-bse-082@cuilahore.edu.pk>
2026-04-23 22:08:00 +0500

    after new domain

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MainCharApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'MainChar',
+      title: 'Inside-UoL',
       debugShowCheckedModeBanner: false,
       initialBinding: BindingsBuilder(() {
         Get.put(AuthController());
